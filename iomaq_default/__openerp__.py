bd72e4eba7ae13bf832ed89068f667adc89f1c9a
jobiols <jorge.obiols@gmail.com>
2018-06-19 09:03:28 -0300

    [ADD] product_autoload

--- iomaq_default/__openerp__.py
+++ iomaq_default/__openerp__.py
@@ -53,6 +53,7 @@
         'account_cash_report',  # reporte de cajas
         'product_upload_prices',  # subir planilla con precios de productos
         'product_prices_update',  # incrementos de precios porcentuales
+        'product_autoload'        # replicacion bulonfer
     ],
 
     'data': [
