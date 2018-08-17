c5adc448a26bc911ff2a0e596d9c6d9de1b93931
jobiols <jorge.obiols@gmail.com>
2018-08-17 16:40:35 -0300

    [ADD] document_page for documentation

--- iomaq_default/__openerp__.py
+++ iomaq_default/__openerp__.py
@@ -57,7 +57,8 @@
         'simple_meli_publishing',  # publicar precios de mercadolibre
         'simple_meshops_publishing',
         'bi_sales_iomaq',           # custom bi report for sales
-        'sale_global_discount' # setea todos los descuentos de la SO
+        'sale_global_discount', # setea todos los descuentos de la SO
+        'document_page'  # documentacion y ayuda
     ],
 
     'data': [
