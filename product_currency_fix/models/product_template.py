1c8016b8fd39f102d349c5bba694ed1b54eb03bd
jobiols <jorge.obiols@gmail.com>
2018-10-01 09:52:38 -0300

    [FIX] product_currency_fix - agregar tests y corregir bug a pasar costo al quant

--- product_currency_fix/models/product_template.py
+++ product_currency_fix/models/product_template.py
@@ -11,35 +11,35 @@ class ProductTemplate(models.Model):
         digits_compute=dp.get_precision('Product Price'),
         groups="base.group_user",
         string="Product Cost",
-        help="Purchase cost in the product currency."
+        help="Historic cost of the product in Product currency."
     )
 
     def fix_quant_data(self, quant, prod, cost):
 
         cc = prod.company_id.currency_id
         if quant:
+            # TODO QUITAR ESTO, no puedo poner costo actual aca
             # si el quant cost esta en cero es critico, le pongo el costo
             # esto no debiera pasar, encima hay que hacerlo con sudo.
-            if not quant.cost or not quant.cost_product:
-                pc = prod.currency_id.with_context(date=quant.in_date)
-                quant.sudo().write({
-                    'cost': pc.compute(cost, cc, round=False),
-                    'cost_product': cost
-                })
+            pass
+            #if not quant.cost or not quant.cost_product:
+            #    pc = prod.currency_id.with_context(date=quant.in_date)
+            #
+            #    quant.sudo().write({
+            #        'cost': pc.compute(cost, cc, round=False),
+            #        'cost_product': cost
+            #    })
 
             # actualizar los standard al precio del quant
-            prod.write({
-                'standard_price': quant.cost,
-                'standard_product_price': quant.cost_product
-            })
+            # TODO No es el lugar para hacer esto.
+            #prod.write({
+            #    'standard_price': quant.cost,
+            #    'standard_product_price': quant.cost_product
+            #})
         else:
             pc = prod.currency_id
-            # no tengo stock le pongo al standard el costo de hoy
+            # no hay quants le pongo al standard el costo de hoy
             prod.write({
                 'standard_price': pc.compute(cost, cc, round=False),
                 'standard_product_price': cost
             })
-
-        # el costo hoy
-        pc = prod.currency_id
-        prod.bulonfer_cost = pc.compute(cost, cc, round=False)
--- product_currency_fix/tests/test_currency.py
+++ product_currency_fix/tests/test_currency.py
@@ -1,35 +1,119 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-from openerp.tests import common
-import os
-import openpyxl
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from __future__ import division
+
+from openerp.tests.common import TransactionCase
 
 #    Forma de correr el test
 #    -----------------------
 #
 #   Definir un subpackage tests que será inspeccionado automáticamente por
-#   modulos de test los modulos de test deben enpezar con test_ y estar
+#   modulos de test los modulos de test deben empezar con test_ y estar
 #   declarados en el __init__.py, como en cualquier package.
 #
-#   Hay que crear una base de datos no importa el nombre (por ejemplo act_test)
-#   vacia y con el modulo que se va a testear instalado (por ejemplo business).
+#   Hay que crear una base de datos no importa el nombre (por ejemplo
+#   bulonfer_test) vacia y con el modulo que se va a testear instalado
+#   (por ejemplo product_autoload).
 #
 #   El usuario admin tiene que tener password admin, Language English, Country
 #   United States.
 #
-#   oe -Q product_currency_fix -c iomaq -d iomaq_test_product_currency_fix
+#   Correr el test con:
+#
+#   oe -Q product_currency_fix -c iomaq -d iomaq_test
 #
 
+import os
 
-class TestCurrencyFix(common.TransactionCase):
+
+class TestBusiness(TransactionCase):
     """ Cada metodo de test corre en su propia transacción y se hace rollback
         despues de cada uno.
     """
 
     def setUp(self):
-        super(TestCurrencyFix, self).setUp()
+        """ Este setup corre antes de cada método ---------------------------00
+        """
+        super(TestBusiness, self).setUp()
+        self.prod_obj = self.env['product.template']
 
-        # crear un producto con currency en dolares
+        account_journal_obj = self.env['account.journal']
+        journal = account_journal_obj.create({
+            'name': 'inventario',
+            'type': 'general',
+            'code': 'INV'
+        })
 
-    def test_01_(self):
-        print 'ok'
+        # categoria para probar inventario permanente producto A1090
+        categ_obj = self.env['product.category']
+        account_obj = self.env['account.account']
+        stock = account_obj.search([('code', '=', '1.1.05.01.010')])
+        input = account_obj.search([('code', '=', '1.1.05.01.020')])
+        output = account_obj.search([('code', '=', '1.1.05.01.030')])
+        categ_data = {
+            'property_stock_account_input_categ_id': input,
+            'property_stock_account_output_categ_id': output,
+            'property_stock_valuation_account_id': stock,
+            'property_cost_method': 'real',
+            'removal_strategy_id': 1,
+            'property_valuation': 'real_time',
+            'property_stock_journal': journal.id
+        }
+        categ = categ_obj.search([('id', '=', 6)])
+        categ.write(categ_data)
+
+        # configurar valuacion de inventario perpetuo
+        self.env['ir.config_parameter'].set_param(
+            'group_stock_inventory_valuation', 1)
+
+    def test_01_perpetual_inventory(self):
+        """ Testear que el costo del producto vaya al quant al ingresar la
+             mercaderia, en las dos monedas, company y product.
+        """
+        cost = 5000.0
+
+        vendor = self.env['res.partner'].search([('name', '=', 'Agrolait')])
+        vendor.ref = 'AGROLAIT'
+
+        # obtener el producto para comprar
+        prod = self.env['product.product'].search(
+            [('default_code', '=', 'A1090')])
+
+        # definir el precio en dolares.
+        prod.product_tmpl_id.force_currency_id = 3
+        prod.product_tmpl_id.set_prices(cost, 'AGROLAIT')
+
+        # crear una orden de compra
+        po = self.env['purchase.order'].create({
+            'partner_id': vendor.id,
+        })
+
+        pol = {
+            'name': '/',
+            'product_id': prod.id,
+            'product_uom': prod.uom_id.id,
+            'price_unit': prod.standard_price,
+            'product_qty': 1,
+            'date_planned': '2018-01-01'
+        }
+        po.order_line = [(0, 0, pol)]
+        po.button_confirm()
+        picking_in = po.picking_ids
+
+        # Confirm and do incoming shipment.
+        picking_in.action_confirm()
+        picking_in.do_transfer()
+
+        # obtener el quant
+        tmpl = prod.product_tmpl_id
+        q = self.prod_obj.oldest_quant(tmpl)
+
+        # company currency ARS
+        cc = vendor.company_id.currency_id
+        # product currency USD
+        pc = prod.currency_id
+
+        self.assertEqual(q.cost, pc.compute(cost, cc))
+        self.assertAlmostEqual(q.cost_product, cost, places=3)
