6a9e5601c31da5a0a1a1f95a0a0e1ff6a4212176
jobiols <jorge.obiols@gmail.com>
2019-05-11 12:12:22 -0300

    [FIX] corregir error en campo ML

--- price_security/models/sale.py
+++ price_security/models/sale.py
@@ -42,7 +42,8 @@ class SaleOrderLine(models.Model):
         price_unit_ml.
         """
         for line in self:
-            unit_price = line.price_unit * (1 + line.price_unit_ml)
+            unit_price = line.price_unit * (
+                1 + line.price_unit_ml or 0.0 / 100.0)
             price = unit_price * (1 - (line.discount or 0.0) / 100.0)
             taxes = line.tax_id.compute_all(price, line.order_id.currency_id,
                                             line.product_uom_qty,
